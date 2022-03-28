b980620c07e5487bea6bba1a4f6fd3fdd9b51b73
Althran <overlord106@gmail.com>
2022-03-28 18:18:19 +0300

    Optimise some problem.(issue #1 and #2)

--- Rock Paper Scissors Game.py
+++ Rock Paper Scissors Game.py
@@ -6,9 +6,10 @@ c = 0
 stack = ['rock', 'scissors', 'paper']
 
 while s == 'y':
+    print('Please, enter rock/scissors/paper')
     player = str(input())
     computer = stack[random.randint(0, len(stack) - 1)]
-    if player == 'rock' and computer == 'rock':
+    if player == computer:
         print('Tie, try again')
     elif player == 'rock' and computer == 'scissors':
         print('Player win')
@@ -19,8 +20,6 @@ while s == 'y':
     elif player == 'scissors' and computer == 'rock':
         print('Computer win')
         c += 1
-    elif player == 'scissors' and computer == 'scissors':
-        print('Tie, try again')
     elif player == 'scissors' and computer == 'paper':
         print('Player win')
         p += 1
@@ -30,8 +29,8 @@ while s == 'y':
     elif player == 'paper' and computer == 'scissors':
         print('Computer win')
         c += 1
-    elif player == 'paper' and computer == 'paper':
-        print('Tie, try again')
+    elif player != 'paper' or 'scissors' or 'rock':
+        print('You write wrong word, please try again.')
     print('Continue? y/n')
     s = input()
 print('Finale score: Player:', p, '/', 'Computer:', c)
